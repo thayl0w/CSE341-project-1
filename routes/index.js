e0495adc1f3f9165f2ea41f0fa9aea30fcb3cdb1
thayl0w <softmardevelopment@gmail.com>
2025-05-14 15:41:48 +0800

    again

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req,res) => {res.send('Hello World');});
+router.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 module.exports = router;
-// 
--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const mongodb = require('./data/database'); 
-
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-mongodb.initDb((err) => {
-    if (err) {
-        console.log('Database connection error:', err);
-    } else {
-        app.listen(port, () => console.log(`Database is connected. Node running on port ${port}`));
-    }
-});
+app.listen(port, () => console.log(`Running on port ${port}`));
